0d8207612d4501d9b46471b3958abc10b594a973
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-12-16 00:16:37 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,56 +54,55 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 	        export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
   
 	        # OTA / DM-Verity / Encryption
-          	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
           	export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
-		      export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-		      export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-		      export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1	
-		      export OF_NO_MIUI_PATCH_WARNING=1
+		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+		export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1	
+		export OF_NO_MIUI_PATCH_WARNING=1
 
-		      export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-		      export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-		      export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-		      export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
-		      export OF_NO_RELOAD_AFTER_DECRYPTION=1
+		export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+		export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+		export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+		export OF_NO_RELOAD_AFTER_DECRYPTION=1
 
 	        # Display / Leds
-		      export OF_SCREEN_H="2460"
-		      export OF_STATUS_H="110"
-		      export OF_STATUS_INDENT_LEFT=48
-		      export OF_STATUS_INDENT_RIGHT=48
-		      export OF_HIDE_NOTCH=1
-		      export OF_CLOCK_POS=1 # left and right clock positions available
-		      export OF_USE_GREEN_LED=0
+		export OF_SCREEN_H="2460"
+		export OF_STATUS_H="110"
+		export OF_STATUS_INDENT_LEFT=48
+		export OF_STATUS_INDENT_RIGHT=48
+		export OF_HIDE_NOTCH=1
+		export OF_CLOCK_POS=1 # left and right clock positions available
+		export OF_USE_GREEN_LED=0
 
-		      export FOX_USE_TAR_BINARY=1
-		      export FOX_USE_SED_BINARY=1
-		      export FOX_USE_XZ_UTILS=1
-		      export OF_FLASHLIGHT_ENABLE=0
+		export FOX_USE_TAR_BINARY=1
+		export FOX_USE_SED_BINARY=1
+		export FOX_USE_XZ_UTILS=1
+		export OF_FLASHLIGHT_ENABLE=0
 
 	        # Security (Disables MTP & ADB during password prompt)
-		      export OF_ADVANCED_SECURITY=1
+		export OF_ADVANCED_SECURITY=1
 
 	        #Debug
-		      export FOX_INSTALLER_DEBUG_MODE=1
+		export FOX_INSTALLER_DEBUG_MODE=1
 
 	        # Metadata encription
         	export OF_FBE_METADATA_MOUNT_IGNORE=1
 
 	        # Removes the loop block errors after flashing ZIPs (Workaround) 
-		      export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+		export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
   
 	        # Other OrangeFox configs
-		      export OF_ENABLE_LPTOOLS=1
-		      export OF_ALLOW_DISABLE_NAVBAR=0
-		      export FOX_DELETE_AROMAFM=1
-		      export OF_USE_SYSTEM_FINGERPRINT=1
+		export OF_ENABLE_LPTOOLS=1
+		export OF_ALLOW_DISABLE_NAVBAR=0
+		export FOX_DELETE_AROMAFM=1
+		export OF_USE_SYSTEM_FINGERPRINT=1
                 
 	        # Backup
-		      export OF_USE_TWRP_SAR_DETECT=1
-		      export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
-		      export OF_QUICK_BACKUP_LIST="/boot;/data;"
-		      export FOX_REPLACE_TOOLBOX_GETPROP=1
+		export OF_USE_TWRP_SAR_DETECT=1
+		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+		export OF_QUICK_BACKUP_LIST="/boot;/data;"
+		export FOX_REPLACE_TOOLBOX_GETPROP=1
 
 	        # run a process after formatting data to work-around MTP issues
 	        export OF_RUN_POST_FORMAT_PROCESS="1"
